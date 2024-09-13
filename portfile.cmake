74d5548be8e71ab0dc8273501b88670acd47cdd7
FacelessTiger <thefacelesstiger@yahoo.com>
2024-09-12 18:30:48 -0600

    Added install to cmake

--- portfile.cmake
+++ portfile.cmake
@@ -4,12 +4,13 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO FacelessTiger/CobraRHI
     REF "${VERSION}"
-    SHA512 9154b0158c6cc5b962f8620888b8965397d1f7d866d80ba62d9be85d4a465a71b30de668d45f5e61fc88a4c2026d58b501cba6b76c8573e70ac77dfc13c992e7
+    SHA512 04aa282fdfa898e462ae7754e7ec74dfcda5920460bce5b884474b2e866c1591dc607dc55d179f64a74a53800cc1e011613df0d6b36d7ecb67b2f5e1fc76a595
     HEAD_REF main
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS -DCOBRARHI_BACKEND=VK
 )
 
 vcpkg_cmake_install()
