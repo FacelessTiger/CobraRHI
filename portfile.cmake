c3a27b2083f611d9f8c8b227a2162cdbed7fa1ba
FacelessTiger <thefacelesstiger@yahoo.com>
2024-09-17 18:20:15 -0600

    fix

--- portfile.cmake
+++ portfile.cmake
@@ -1,11 +1,9 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
-vcpkg_from_github(
+vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO FacelessTiger/CobraRHI
-    REF "${VERSION}"
-    SHA512 5522c28c290014c8fc2094ad3622ced9eddd129e3f5edd348fc5529745d3bf8eb376d62672087b6624a6c4e209f950811e1abb0f327dbc91dc8e0a3865144ce6
-    HEAD_REF main
+    URL https://github.com/FacelessTiger/CobraRHI
+    REF a35c3391f10f55e651dc875fd718a19955fe07a9
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
